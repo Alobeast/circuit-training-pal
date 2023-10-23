c25da8788560cc62db17aef23074e469aeeb2150
Alobeast <alois.beard@gmail.com>
2023-10-23 15:04:20 +0200

    Add CRUD broadcasting to the Session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,12 @@ class Session < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
+  # after_create_commit -> { broadcast_prepend_to "sessions", partial: "sessions/session", locals: { session: self }, target: "sessions" }
+  # if following naming conventions the above line can be abbreviated to the following line :
+  # after_create_commit -> { broadcast_prepend_later_to "sessions" }
+  # after_update_commit -> { broadcast_replace_later_to "sessions" }
+  # after_destroy_commit -> { broadcast_remove_to "sessions" }
+
+  # again if following conventions the 3 lines above are equivalent to the following
+  broadcasts_to ->(session) { "sessions" }, inserts_by: :prepend
 end
